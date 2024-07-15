9c6ea23e2f5da07798cfd3bac075c319d31b1d57
Miguel <128045563+miguellrp@users.noreply.github.com>
2024-07-15 12:51:34 +0200

    🛠️ Se preparan los archivos de configuración responsables para desplegar en Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 import { defineConfig } from 'astro/config';
 import { defaultLang } from './src/i18n/dictionary';
+import netlify from "@astrojs/netlify";
 
+// https://astro.build/config
 export default defineConfig({
   i18n: {
     defaultLocale: "es",
-    locales: ["es", "en", "gl"],
+    locales: ["es", "en", "gl"]
   },
   // TODO: redirect checking current language
   redirects: {
     '/': `/${defaultLang}`
-  }
+  },
+  output: "server",
+  adapter: netlify()
 });
